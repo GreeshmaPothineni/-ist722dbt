b825c5a63af79a8e621db4200e6ff449130b11ac
Greeshma Pothineni <gpothine@g.syr.edu>
2024-03-08 03:10:41 +0000

    Finished Assignment F

--- northwind/models/dim_products.sql
+++ northwind/models/dim_products.sql
@@ -7,4 +7,4 @@ stg_categories as (
 select  {{ dbt_utils.generate_surrogate_key(['p.productid']) }} as productkey, 
     p.productid,p.productname,p.supplierid,c.categoryname,c.description as categorydescription
     from stg_products p
-    join stg_categories c on p.categoryid = c.categoryid
+    join stg_categories c on p.categoryid  =  c.categoryid
